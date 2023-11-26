030cd4f7263bd3214c9990f862dac732d2c1adf6
oleg-olegi <ol.shink87@gmail.com>
2023-11-26 19:47:06 +0300

    Test

--- src/test/java/com/example/coursework2/service/JavaQuestionServiceTest.java
+++ src/test/java/com/example/coursework2/service/JavaQuestionServiceTest.java
@@ -1,16 +1,18 @@
 package com.example.coursework2.service;
 
 import com.example.coursework2.Question;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collection;
+import java.util.Random;
 import java.util.stream.Stream;
 
 
@@ -20,9 +22,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class JavaQuestionServiceTest {
+    @InjectMocks
     private JavaQuestionService javaQuestionService;
 
     private Question questionMock = mock(Question.class);
+
+    private Random random = mock(Random.class);
 
     @BeforeEach
     void setJavaQuestionService() {
@@ -31,6 +36,13 @@ public class JavaQuestionServiceTest {
 
     static Stream<Arguments> argumentsStream() {
         return Stream.of(Arguments.of("Question", "Answer"));
+    }
+
+    static Stream<Arguments> questionsProvider() {
+        return Stream.of(Arguments.of(
+                new Question("Question1", "Answer1"),
+                new Question("Question2", "Answer2"),
+                new Question("Question3", "Answer3")));
     }
 
     @ParameterizedTest
@@ -62,6 +74,36 @@ public class JavaQuestionServiceTest {
         assertTrue(questionSet.isEmpty());
         assertFalse(questionSet.contains(questionToRemove));
         assertThrows(IllegalArgumentException.class, () -> javaQuestionService.remove(questionToRemove));
+    }
+
+    @ParameterizedTest
+    @MethodSource("questionsProvider")
+    void getAllTest(Question question1, Question question2, Question question3) {
+        javaQuestionService.add(question1);
+        javaQuestionService.add(question2);
+        javaQuestionService.add(question3);
+        Collection<Question> questions = javaQuestionService.getAll();
+        assertTrue(
+                questions.contains(question1) &&
+                        questions.contains(question2) &&
+                        questions.contains(question3));
+        assertEquals(3, questions.size());
+    }
+
+    @ParameterizedTest
+    @MethodSource("questionsProvider")
+    void getRandomQuestionTest(Question question1, Question question2, Question question3) {
+        assertThrows(RuntimeException.class, () -> javaQuestionService.getRandomQuestion());
+        javaQuestionService.add(question1);
+        javaQuestionService.add(question2);
+        javaQuestionService.add(question3);
+        when(random.nextInt(3)).thenReturn(1);
+        Question randomQuestion = javaQuestionService.getRandomQuestion();
+        Collection<Question> collection = javaQuestionService.getAll();
+        assertNotNull(randomQuestion);
+        assertTrue(collection.contains(question1) ||
+                collection.contains(question2) ||
+                collection.contains(question3));
     }
 
     // Переопределение equals и hashCode в мок-объекте Question
