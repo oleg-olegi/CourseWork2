8fc5cb94217e2793f5df8c221e7392bba523a252
oleg-olegi <ol.shink87@gmail.com>
2023-11-26 10:59:43 +0300

    ExamController, ExaminerServiceImpl

---
+++ src/main/java/com/example/coursework2/controller/ExamController.java
@@ -0,0 +1,23 @@
+package com.example.coursework2.controller;
+
+import com.example.coursework2.Question;
+import com.example.coursework2.service.ExaminerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/exam")
+@AllArgsConstructor
+public class ExamController {
+    private ExaminerService service;
+
+    @GetMapping("/get/{amount}")
+    public Collection<Question> getQuestions(@PathVariable int amount) {
+        return service.getQuestions(amount);
+    }
+}
--- src/main/java/com/example/coursework2/service/ExaminerServiceImpl.java
+++ src/main/java/com/example/coursework2/service/ExaminerServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.coursework2.service;
 
 import com.example.coursework2.Question;
+import com.example.coursework2.exceptions.AmountMoreThanQuestionsQuantityException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
-
+@Service
 public class ExaminerServiceImpl implements ExaminerService {
     private Random random;
     private QuestionService questionService;
@@ -18,8 +21,29 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public Collection<Question> getQuestions(int amount) {
-        List<Question> questionList = new ArrayList<>(amount);
+        List<Question> questionListForExam = new ArrayList<>(amount);//список для хранения вопросов для экзамена
+        Collection<Question> allQuestions = questionService.getAll();//список всех вопросов
+        List<Question> questionList = new ArrayList<>(allQuestions);//список всех вопросов, приведенный к лист
+        int totalQuestions = allQuestions.size();
+        if (amount > totalQuestions) {
+            throw new AmountMoreThanQuestionsQuantityException("Запрос превышает фактическое количество содержимого");
+        }
+        for (int i = 0; i < amount; i++) {
+            Question randomQuestion = getRandomUniqueQuestion(questionList, questionListForExam);
+            questionListForExam.add(i, randomQuestion);
+            questionList.remove(randomQuestion);
+        }
+        return questionListForExam;
+    }
 
-        return null;
+    private Question getRandomUniqueQuestion(List<Question> questions, List<Question> questionListForExam) {
+        int totalQuestions = questions.size();
+        while (true) {
+            int randomIndex = random.nextInt(totalQuestions);
+            Question selectedQuestion = questions.get(randomIndex);
+            if (!questionListForExam.contains(selectedQuestion)) {
+                return selectedQuestion;
+            }
+        }
     }
 }
